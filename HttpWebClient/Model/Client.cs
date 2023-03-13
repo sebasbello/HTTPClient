1be466c90a21ac70e2d62274517e04e59bcf0fd5
sebastianbello <sebtrejo@outlook.es>
2023-03-13 09:34:55 -0600

    Upload HttpClient/

---
+++ HttpWebClient/Model/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpWebClient.Model
+{
+    public static class Client
+    {
+        public static async Task<HttpResponseMessage> executeRequest(string url, string method)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                HttpRequestMessage request = null;
+                HttpResponseMessage response = null;
+                switch (method)
+                {
+                    case "GET":
+                        request = new HttpRequestMessage(HttpMethod.Get, url);
+                        response = await httpClient.SendAsync(request);
+                        break;
+                    case "HEAD":
+                        request = new HttpRequestMessage(HttpMethod.Head, url);
+                        response = await httpClient.SendAsync(request);
+                        break;
+                    case "OPTIONS":
+                        request = new HttpRequestMessage(HttpMethod.Options, url);
+                        response = await httpClient.SendAsync(request);
+                        break;
+                    case "POST":
+                        break;
+                    case "PUT":
+                        break;
+                    case "PATCH":
+                        break;
+                    case "DELETE":
+                        break;
+                }
+                return response;
+            }
+        }
+    }
+}
