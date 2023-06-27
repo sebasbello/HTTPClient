2230cf2296af798d2145e66dc9fe153e6569a1a0
Sebastian Bello <sebtrejo@outlook.es>
2023-06-26 23:05:49 -0600

    Repository refactoring

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# HTTPClient
-HTTP client implementation using C#.
+# C# API Client with WPF
+
+This repository contains a collection of API client written in C# and integrated with the Windows Presentation Foundation (WPF) framework.
+
+## Key Features
+
+- **Versatile API Support:** The repository offers a wide range of pre-built API clients for popular web services, including social media platforms, cloud storage providers, payment gateways, and more. Each client is tailored to the specific API, ensuring compatibility and providing a consistent interface for developers.
+
+- **C# Language:** All API clients are developed using C#, a powerful and widely-used programming language in the .NET ecosystem. C# offers a rich set of features, including strong typing, object-oriented programming, and seamless integration with other .NET technologies.
+
+- **Windows Presentation Foundation (WPF) Integration:** The API clients are designed to work seamlessly within WPF applications. WPF provides a robust framework for building desktop applications with rich UI elements, data binding capabilities, and a flexible layout system. By integrating the API clients with WPF, developers can create visually appealing and interactive applications that interact with various APIs.
+
+- **Easy-to-Use Interface:** The API clients in this repository are designed with simplicity and ease of use in mind. They provide intuitive methods and classes for handling authentication, making API requests, processing responses, and handling errors.
+
+## Installation and Usage
+
+To use the API clients from this repository in your WPF project, follow these steps:
+
+1. Clone the repository to your local machine or download the source code as a ZIP file.
+
+2. Open your WPF project in your preferred Integrated Development Environment (IDE), such as Visual Studio.
+
+3. Add the API client library files to your project. You can either reference the library project directly or include the individual API client files.
+
+4. Review the documentation and example code provided in the repository to understand how to utilize the specific API clients. Each client may have unique usage instructions, authentication requirements, and methods for making API requests.
+
+5. Import the required namespaces and instantiate the API client within your WPF application code.
+
+6. Begin utilizing the API client by calling its methods to interact with the corresponding API. This may include making GET or POST requests, handling responses, and processing data.
+
+## Contribution Guidelines
+
+Contributions to this repository are welcome!
+
+Please ensure that any code contributions adhere to the repository's coding conventions, such as code formatting, naming conventions, and appropriate documentation. By maintaining consistent coding practices, we can collectively improve the quality and usability of the API client library.
+
+
+## Support and Feedback
+
+If you encounter any issues, have questions, or would like to provide feedback, please create an issue in the repository's issue tracker. The maintainers of this repository will actively review and respond to any inquiries, bug reports, or suggestions.
+
+We hope that this repository simplifies the process of integrating APIs into your WPF applications and empowers you to build
---
+++ ResponseClient/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ResponseClient.Model;
+using System.Net.Mime;
+using System.IO;
+using Microsoft.Web.WebView2.Wpf;
+using MimeTypes;
+using Microsoft.Win32;
+using HeyRed.Mime;
+
+namespace ResponseClient
+{
+    public partial class MainWindow : Window
+    {
+        string content_type = "";
+        Stream content_binary = null;
+        Image image = null;
+        string content_text = "";
+        string mimeExtension = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txt_url.Text))
+            {
+                this.raw_radioButton.IsChecked = false;
+                this.pretty_radioButton.IsChecked = false;
+                this.tab_control.SelectedIndex = 0;
+                this.tab_body.IsEnabled = false;
+                this.bdr_indicator.BorderBrush = null;
+                this.txt_headers.Text = "";
+                this.lbl_statuscode.Content = "";
+
+                try
+                {
+                    if (!txt_url.Text.ToLower().StartsWith("http") && !txt_url.Text.ToLower().StartsWith("https"))
+                    {
+                        txt_url.Text = "https://" + txt_url.Text;
+                    }
+                    this.bdr_indicator.BorderBrush = new SolidColorBrush(Colors.Blue);
+                    this.lbl_statuscode.Content = "Processing request...";
+
+                    HttpResponseMessage response = await Client.executeRequest(txt_url.Text, cbx_method.Text);
+                    HttpStatusCode statusCode = response.StatusCode;
+                    this.lbl_statuscode.Content = "" + (int)statusCode + " - " + statusCode.ToString();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        this.bdr_indicator.BorderBrush = new SolidColorBrush(Colors.Green);
+                        this.raw_radioButton.IsEnabled = true;
+                        this.pretty_radioButton.IsEnabled = true;
+                    }
+                    else
+                    {
+                        this.bdr_indicator.BorderBrush = new SolidColorBrush(Colors.Red);
+                    }
+
+                    this.txt_headers.Text += "****** General/Response: ******\r\n\r\n";
+                    this.txt_headers.Text += response.Headers.ToString();
+                    this.txt_headers.Text += "\r\n";
+                    this.txt_headers.Text += "****** Entity: ******\r\n\r\n";
+                    this.txt_headers.Text += response.Content.Headers.ToString();
+                    content_text = await response.Content.ReadAsStringAsync();
+                    content_binary = await response.Content.ReadAsStreamAsync();
+                    content_type = response.Content.Headers.ContentType.ToString();
+                    this.lbl_content_type.Content = content_type;
+                    mimeExtension = "*." + MimeTypesMap.GetExtension(content_type);
+                    content_binary.Position = 0;
+                    Byte[] buffer = new byte[content_binary.Length];
+                    content_binary.Read(buffer, 0, buffer.Length);
+
+                    image = new Image();
+                    image.Source = ConvertByteArrayToBitmapImage(buffer);
+                }
+                catch (Exception exception)
+                {
+                    this.bdr_indicator.BorderBrush = new SolidColorBrush(Colors.Red);
+                    this.lbl_statuscode.Content = exception.Message;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Add URL for search.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void RawRadioButton_Check(object sender, RoutedEventArgs e)
+        {
+            TextBlock txt_block = new TextBlock();
+            txt_block.TextWrapping = TextWrapping.Wrap;
+            txt_block.Text = content_text;
+
+            this.tab_body.IsEnabled = true;
+            this.sview_body.Content = null;
+            this.sview_body.Content = txt_block;
+        }
+
+        private void PrettyRadioButton_Check(object sender, RoutedEventArgs e)
+        {
+            if (content_type.StartsWith("text/html"))
+            {
+                WebView2 webView2 = new WebView2();
+                webView2.Source = new Uri(txt_url.Text);
+
+                this.tab_body.IsEnabled = true;
+                this.sview_body.Content = null;
+                this.sview_body.Content = webView2;
+            }
+            else if (content_type.StartsWith("image"))
+            {
+                this.tab_body.IsEnabled = true;
+                this.sview_body.Content = null;
+                this.sview_body.Content = image;
+            }
+        }
+
+        public BitmapImage ConvertByteArrayToBitmapImage(Byte[] bytes)
+        {
+            Stream strem = new MemoryStream(bytes);
+            strem.Seek(0, SeekOrigin.Begin);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = strem;
+            image.EndInit();
+            return image;
+        }
+
+        public void Save(SaveFileDialog saveFileDialog)
+        {
+            string fileName = saveFileDialog.FileName;
+            if (fileName != null)
+            {
+                if (content_type.StartsWith("text/html"))
+                {
+                    StreamWriter streamWriter = new StreamWriter(File.Create(saveFileDialog.FileName));
+                    streamWriter.Write(content_text);
+                    streamWriter.Dispose();
+                }
+                else if (content_type.StartsWith("image"))
+                {
+                    var pngBitmapEncoder = new PngBitmapEncoder();
+                    pngBitmapEncoder.Frames.Add(BitmapFrame.Create((BitmapSource)image.Source));
+                    using (FileStream stream = new FileStream(fileName, FileMode.Create))
+                    {
+                        pngBitmapEncoder.Save(stream);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File extension not expected. Please, try again.", "Warning",
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Title = "Save",
+                Filter = (content_type.StartsWith("text/html")) ? "(*.html)|*.html" : "(" + mimeExtension + ")|" + mimeExtension,
+                FilterIndex = 0
+            };
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != null)
+            {
+                Save(saveFileDialog);
+            }
+        }
+    }
+}
